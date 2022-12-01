71476c9d148d717156b56840038a2936dd54a533
OSEREME <94149337+xreme@users.noreply.github.com>
2022-11-30 20:09:49 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,87 @@
+﻿
+document.getElementById("logo").src="logo.png";
+
+document.getElementById("p1").innerHTML="Dogs have been selectively bred for thousands of years, sometimes by inbreeding dogs from the same ancestral lines, sometimes by mixing dogs from very different lines.[1] The process continues today, resulting in a wide variety of breeds, hybrids, and types of dog. Centuries of selective breeding by humans has resulted in dogs being more genetically diverse than most other mammals by a considerable margin. As such, dogs are the only animal with such a wide variation in appearance without speciation, from the Chihuahua to the Great Dane.[2] ";
+
+document.getElementById("p2").innerHTML="The following list uses a wide interpretation of breed. Breeds are usually categorized by the functional type from which the breed was developed. The basic types are companion dogs, guard dogs, hunting dogs, herding dogs, and working dogs, although there are many other types and subtypes. \"Breeds listed\" here may be traditional breeds with long histories as registered breeds, rare breeds with their own registries, or new breeds that may still be under development. ";
+
+
+document.getElementById("dogs").src="dog.jpg";
+
+document.getElementById("p3").innerHTML="In some cases, a breed's origin overlaps the boundaries of two or more countries; the dog is normally listed only in the country with which it is most commonly associated (for example, by its designated country according to the Federation Cynologique Internationale (FCI). Some dogs, such as the Lowchen, have an uncertain origin and are listed under several countries. ";
+
+
+
+document.getElementById("large_dogs").src="logo_large.png";
+
+document.getElementById("Alaska").src="alaskanmalamute.jpg";
+document.getElementById("Boxer").src="boxer.jpg";
+document.getElementById("Dober").src="doberman.jpg";
+document.getElementById("Rot").src="rotti.jpg";
+document.getElementById("German").src="german.jpg";
+
+function Alaska(){ 
+
+document.getElementById("display").src="alaskanmalamute2.jpg";
+
+document.getElementById("p4").innerHTML= "The American Kennel Club (AKC) breed standard describes a natural range of size, with a desired size of 23 inches (58 cm) tall and 75 pounds (34 kg) for females, 25 inches (64 cm) tall and 85 pounds (39 kg) for males.[1] Heavier individuals (90 lb (41 kg)) and dogs smaller than 75 pounds (34 kg) are commonly seen. There is often a marked size difference between males and females. Weights upwards of 100 pounds (45 kg) are also seen.[1]The coat of the Alaskan Malamute is a double coat. The undercoat has an oily and woolly texture and can be as thick as two inches.[1] The outer guard coat is coarse and stands off the body—longer at the withers but not more than one inch off the sides of the body. Ears are small in proportion to the head and stand firmly erect when at attention. The Alaskan Malamute is a heavy dog, with a more formidable nature and structure than the Siberian Husky, which is bred for speed. The Alaskan Malamute is bred for power and endurance, which is its original function and what the standard of the breed requires of Alaskan Malamute breeders.";
+
+document.getElementById("p5").innerHTML= "The usual colors are various shades of gray and white, sable and white, black and white, seal and white, red and white, or solid white.[2] There are a wide range of markings in the breed including face markings, blazes, a splash at the nape of the neck, and a collar or half collar. In terms of color variants, some Malamutes exhibit a dark grey to buff-colored undertone around their trimmings and white areas, presenting with a color-linked gene known as Agouti. The eyes of the Alaskan Malamute are almond-shaped and are varied shades of brown; however, the darker eye is preferred. The physical build of the Malamute is compact and strong with substance, bone and snowshoe feet.";
+
+}
+
+
+function Box(){
+
+document.getElementById("display").src="boxer2.jpg";
+
+document.getElementById("p4").innerHTML= "The Boxer is a medium-sized, short-haired breed of dog, developed in Germany. The coat is smooth and tight-fitting. Colours are fawn or brindled, with or without white markings, and white. Boxers are brachycephalic (they have broad, short skulls), have a square muzzle, mandibular prognathism (an underbite), very strong jaws, and a powerful bite ideal for hanging on to large prey. The Boxer was bred from the Old English Bulldog and the now extinct Bullenbeisser which became extinct by crossbreeding rather than by a decadence of the breed. The purpose of the crossbreeding was the wish to eliminate the excessive white color of the breed, and the necessity of producing thousands of dogs for one of the most popular breeds in the world. The Boxer is part of the Molosser group. This group is a category of solidly built, large dog breeds that all descend from the same common ancestor, the large shepherd dog known as a Molossus. The Boxer is a member of the Working Group. ";
+
+document.getElementById("p5").innerHTML= "The first Boxer club was founded in 1895, with Boxers being first exhibited in a dog show for St. Bernards in Munich the next year. Based on 2013 American Kennel Club statistics, Boxers held steady as the seventh-most popular breed of dog in the United States for the fourth consecutive year. However, according to the AKC's website, the boxer is now the eighth-most popular dog breed in the United States. ";
+
+}
+
+
+function Dober(){
+
+document.getElementById("display").src="doberman2.jpg";
+
+document.getElementById("p4").innerHTML= "The Doberman Pinscher or Dobermann, or Doberman, is a medium-large breed of domestic dog originally developed around 1890 by Karl Friedrich Louis Dobermann, a tax collector from Germany. The muzzle is long, and so affords the leverage for an extremely strong bite. The Doberman stands on its toesand is not usually heavy-footed. Ideally, they have an even and graceful gait. Traditionally, the ears are cropped and posted, and the tail is docked. However, in some countries it is illegal to do so. Except in the albino, in which it is extremely difficult to see, each purebred Doberman has markings on the chest, paws/legs, muzzle, above the eyes, and underneath the tail. ";
+
+document.getElementById("p5").innerHTML= "Doberman Pinschers are well known as intelligent, alert, and tenaciously loyal companions and guard dogs. Personality varies a great deal between each Doberman, but if taken care of and trained properly they tend to be loving and devoted companions. The Doberman is driven, strong, and sometimes stubborn. Owning one requires commitment and care, but if trained well, they can be wonderful family dogs. Unlike some breeds, Dobermans are eager to please only after their place is established in their pack and that place is not as an alpha. With a consistent approach they can be easy to train and will learn very quickly. As with all dogs, if properly trained, they can be excellent with children. Dobermans adapt quickly, though they take their cue from their leader and value attention.";
+
+}
+
+
+function Rot(){
+
+document.getElementById("display").src="rotti2.jpg";
+
+document.getElementById("p4").innerHTML= "The Rottweiler is a breed of domestic dog, regarded as medium-to-large or large. The dogs were known in German as Rottweiler Metzgerhund, meaning Rottweil butchers dogs, because one of their uses was to herd livestock and pull carts laden with butchered meat to market. This continued until the mid-19th century when railways replaced droving for herding. Rottweilers are now used as search and rescue dogs, as guide dogs for the blind, as guard dogs and police dogs. ";
+
+document.getElementById("p5").innerHTML= "Rottweiler breeders aim at a dog of abundant strength, black coated with clearly defined rich tan markings, whose powerful appearance does not lack nobility and which is exceptionally well suited to being a companion, service and working dog. This breed is all about balance, endurance, proportionality, intelligence and strength. The various standards in place for the Rottweilers physical appearance specify these characteristics. ";
+
+}
+
+
+function Ger(){
+
+
+document.getElementById("display").src="German.jpg";
+
+document.getElementById("p4").innerHTML= "The German Shepherd is a breed of medium to large-sized working dog that originated in Germany. The breed's officially recognized name is German Shepherd Dog in the English language, sometimes abbreviated as GSD and was also formerly known as the Alsatian in Britain. The German Shepherd is a relatively new breed of dog, with their origin dating to 1899. As part of the Herding Group, German Shepherds are working dogs developed originally for herding sheep. Since that time, however, because of their strength, intelligence, trainability, and obedience, German Shepherds around the world are often the preferred breed for many types of work, including disability assistance, search-and-rescue, police and military roles, and even acting. The German Shepherd is the second-most popular breed of dog in the United Statesand fourth-most popular in the United Kingdom. ";
+
+document.getElementById("p5").innerHTML= "German Shepherds were bred specifically for their intelligence, a trait for which they are now famous. In the book The Intelligence of Dogs, author Stanley Coren ranked the breed third for intelligence, behind Border Collies and Poodles. He found that they had the ability to learn simple tasks after only five repetitions and obeyed the first command given 95% of the time. Coupled with their strength, this trait makes the breed desirable as police, guard and search and rescue dogs, as they are able to quickly learn various tasks and interpret instructions better than other breeds. ";
+
+}
+
+
+
+
+
+
+
+
+
+
+
